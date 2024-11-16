f4288d8dad6d93a06318964794c2f52295b2cac4
Anayo Sylvester <sylvesteranayo2@gmail.com>
2024-11-16 14:59:37 +0100

    fixed promise return instead of function

--- middlewares/async-wrapper.js
+++ middlewares/async-wrapper.js
@@ -1,9 +1,7 @@
-const asyncWrapper = async(fn)=>{
-  try{
-    await fn(req,res,next);
-  }catch(err){
-    next(err);
-  }
-}
+const asyncWrapper = (fn) => {
+  return (req, res, next) => {
+      fn(req, res, next).catch(next); // Pass errors to Express's error handler
+  };
+};
 
 module.exports = asyncWrapper;
