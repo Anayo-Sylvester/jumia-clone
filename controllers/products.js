43200639e16cb0bbd9b7d7f868b089d0a1a7962d
Anayo Sylvester <sylvesteranayo2@gmail.com>
2024-11-18 20:58:32 +0100

    searxh querries and parameters added also categories params

--- controllers/products.js
+++ controllers/products.js
@@ -7,11 +7,52 @@ const Product = require('../models/model')
 
 
 const getAllProduct = asyncWrapper(
-  async(req,res)=>{
-    const products = await Product.find({});
-    res.status(200).json({nhBITS: products.length, data:{products}})
+  async (req, res) => {
+    const { categoryName } = req.params;
+    let { page, limit, search,sort } = req.query;
+    let restructuredSort;
+
+    const filterObject = {};
+
+    // Build filter object based on query params
+    if (categoryName) {
+      filterObject.category = categoryName;
+    }
+    if (search) {
+      filterObject.name = { $regex: search, $options: "i" }; // Case-insensitive search
+    }
+    if(sort){
+      restructuredSort = sort.split(',').join(' ');
+    }
+
+    // Set default values for pagination
+    page = Number(page) || 1;
+    limit = Number(limit) || 10;
+
+    console.log(filterObject);
+    console.log(req.query);
+
+    // Get the total count of matching documents
+    const totalCount = await Product.countDocuments(filterObject);
+
+    // Get the paginated data
+    const products = await Product.find(filterObject)
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .sort(restructuredSort||{})
+
+    // Send the response with total count and paginated data
+    res.status(200).json({
+      totalItems: totalCount, // Total matching documents
+      page,
+      limit,
+      itemsOnPage: products.length, // Number of items on this page
+      data: products,
+    });
   }
 );
+
+
 
 const getSingleProduct = asyncWrapper(
   async(req,res)=>{
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const {getAllProduct,getSingleProduct,createProduct,updateProduct,deleteProduct} = require('../controllers/products')
 
 router.route('/').get(getAllProduct).post(createProduct)
+router.route('/category/:categoryName').get(getAllProduct)
 router.route('/:id').get(getSingleProduct).patch(updateProduct).delete(deleteProduct);
 
 module.exports = router
