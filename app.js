b72f87c9bdd7e7f424692ad082751104a5270ef7
Anayo Sylvester <sylvesteranayo2@gmail.com>
2024-11-20 20:45:10 +0100

    resolved cors issue on frontend

--- app.js
+++ app.js
@@ -4,12 +4,16 @@ const handleErrorMiddleware = require('./middlewares/handleErrorMiddleware');
 const express = require('express');
 const router = require('./routes/routes');
 const notFound = require('./middlewares/notFound');
+
 const app = express();
+const cors = require('cors');
+
 const port = process.env.PORT||5000;
 require('dotenv').config();
 
 //middlewares
 app.use(express.json())
+app.use(cors()); // Enable CORS for all routes and origins
 
 //routes
 app.use('/api/v1/products',router)
