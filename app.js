9e351fcfa7bd0a3409d64d3b79530d92bad8ba0f
Anayo Sylvester <sylvesteranayo2@gmail.com>
2024-12-07 18:25:23 +0100

    converted yaml to json and removed yamljs

--- app.js
+++ app.js
@@ -17,8 +17,7 @@ const helmet = require('helmet');
 const {rateLimit} = require('express-rate-limit');
 
 const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const swaggerDocument = YAML.load('./swagger.yaml');
+const swaggerDoc = require('./swagger.json');
 
 const port = process.env.PORT||5000;
 require('dotenv').config();
@@ -43,7 +42,7 @@ app.use(express.json())
 app.get('/',(req,res)=>{
   res.send('<h1>jumia clone api</h1><a href="/api-docs">Documentation</a>')
 })
-app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs',swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 app.use('/api/v1/products',productsRoutes);
 app.use('/api/v1/auth',authRoutes);
 app.use('/api/v1/cart',Authenticator,cartRoutes);
